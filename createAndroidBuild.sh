0c3623f25f9293f0aa5b1edf703d1e1dac1caaaf
Voon-Li CHUNG <voonli.chung@gaiaresources.com.au>
2020-10-26 14:21:01 +0800

    Fixed formatting error

--- createAndroidBuild.sh
+++ createAndroidBuild.sh
@@ -4,7 +4,7 @@ ANDROID_HOME=/home/vlchung/Android/Sdk/
 BUILD_TOOL_VERSION=`ls $ANDROID_HOME/build-tools | head -1l`
 # Cordova is so far behind the latest, that we need the earliest version on the system.
 PATH=$PATH:/home/vlchung/.gradle/wrapper/dists/gradle-5.4.1-all/3221gyojl5jsh0helicew7rwx/gradle-5.4.1/bin/
-ionic cordova build --release android
+ionic cordova compile --release android
 ~/Android/Sdk/build-tools/$BUILD_TOOL_VERSION/zipalign -v -p 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $$.1.apk
 ~/Android/Sdk/build-tools/$BUILD_TOOL_VERSION/apksigner sign --ks my-release-key.jks --out `date +"%Y%m%d-%H%M%S"`.apk $$.1.apk
 rm $$.1.apk
